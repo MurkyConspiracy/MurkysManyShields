c0c3e44011a7f7b0de12390427b9fefc39f1fade
MurkyConspiracy <nickreeder43@gmail.com>
2020-01-27 10:29:06 -0600

    Minor Json changes

--- src/main/java/murkyconspiracy/shieldmod/item/natural/GrassShield.java
+++ src/main/java/murkyconspiracy/shieldmod/item/natural/GrassShield.java
@@ -20,7 +20,7 @@ import net.minecraft.world.World;
 
 public class GrassShield extends Item {
 	
-	static Properties properties = new Properties().group(ShieldMod.shieldmodIG).maxDamage(150);
+	static Properties properties = new Properties().group(ShieldMod.shieldmodIG).maxDamage(35);
 	public GrassShield() {
 		super(properties);
 		this.setRegistryName(new ResourceLocation(ShieldMod.modid, "grassshield"));
